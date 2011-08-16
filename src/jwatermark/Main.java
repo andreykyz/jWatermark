0b86b99ab47991b458d4d7c534b1317e30c0cc87
andreykyz <andreykyz@gmail.com>
2011-08-16 16:30:48 +0400

    move my files to new package

--- src/jwatermark/Main.java
+++ src/jwatermark/Main.java
@@ -1,24 +1,10 @@
 package jwatermark;
 
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-
 
 public class Main {
 
 	public static void main(String[] args) {
-//		new ImageWatermarkGui().run();
-		try {
-	          UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-	      } catch (Exception ex) {
-	          ex.printStackTrace();
-	      }
-				
-			SwingUtilities.invokeLater(new Runnable() {
-	            public void run() {
-	            	new ImageWatermarkGuiSwing().setVisible(true);
-	            }
-	        });
+
 	}
 
 }
