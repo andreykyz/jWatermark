740965b844f82669be1088f06f17bf06bfb5a565
andreykyz <andreykyz@gmail.com>
2011-08-16 16:31:21 +0400

    create new package

---
+++ src/ru/devhead/jwatermark/Main.java
@@ -0,0 +1,25 @@
+package ru.devhead.jwatermark;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+	          UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+	      } catch (Exception ex) {
+	          ex.printStackTrace();
+	      }
+				
+			SwingUtilities.invokeLater(new Runnable() {
+	            public void run() {
+	            	new ImageWatermarkGuiSwing().setVisible(true);
+	            }
+	        });
+	}
+
+}
