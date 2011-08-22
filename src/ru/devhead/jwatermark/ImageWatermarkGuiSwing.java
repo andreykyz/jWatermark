d1b7176d163dbfa08cc040f98f780a9612ec4694
andreykyz <andreykyz@gmail.com>
2011-08-22 15:54:36 +0400

    Save button work

--- src/ru/devhead/jwatermark/ImageWatermarkGuiSwing.java
+++ src/ru/devhead/jwatermark/ImageWatermarkGuiSwing.java
@@ -1,11 +1,15 @@
 package ru.devhead.jwatermark;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.File;
 
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -13,6 +17,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class ImageWatermarkGuiSwing extends JFrame {
 
@@ -20,7 +25,11 @@ public class ImageWatermarkGuiSwing extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	JTextField sourceImagePath;
+	JTextField watermarkPath;
+	JPanel panel;
+	
 	ImageWatermarkGuiSwing() {
 
 		JMenuItem miExit = new JMenuItem("Exit");
@@ -42,19 +51,64 @@ public class ImageWatermarkGuiSwing extends JFrame {
 		JLabel watermarkImageLabel = new JLabel("Watermark image:");
 		JLabel watermarkTypeLabel = new JLabel("Watermark image:");
 		
-		JTextField sourceImagePath = new JTextField();
-		JTextField watermarkPath = new JTextField();
+		sourceImagePath = new JTextField();
+		watermarkPath = new JTextField();
 		
-		
-
 		JButton browseSourceButton = new JButton("Browse");
+		browseSourceButton.addActionListener(new ActionListener() {
+			@Override
+	        public void actionPerformed(ActionEvent e) {
+	        	JFileChooser chooser = new JFileChooser();
+	            FileNameExtensionFilter filter = new FileNameExtensionFilter(
+	                "*.jpg, *.jpeg, *.gif, *.png", "jpg", "gif", "jpeg", "png");
+	            chooser.setFileFilter(filter);
+	            int returnVal = chooser.showOpenDialog(panel);
+	            if(returnVal == JFileChooser.APPROVE_OPTION) {
+	       	        	sourceImagePath.setText(chooser.getSelectedFile().getAbsolutePath());
+	            }
+
+
+	            }
+	    });
+		
 		JButton browseWatermarkButton = new JButton("Browse");
+		browseWatermarkButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+	        	JFileChooser chooser = new JFileChooser();
+	            FileNameExtensionFilter filter = new FileNameExtensionFilter(
+	                "*.jpg, *.jpeg, *.gif, *.png", "jpg", "gif", "jpeg", "png");
+	            chooser.setFileFilter(filter);
+	            int returnVal = chooser.showOpenDialog(panel);
+	            if(returnVal == JFileChooser.APPROVE_OPTION) {
+	            	watermarkPath.setText(chooser.getSelectedFile().getAbsolutePath());
+	            }
+			}
+			
+		});
 		JButton saveButton = new JButton("Save");
+		saveButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (!sourceImagePath.equals("") | !watermarkPath.equals("")) {
+					JFileChooser chooser = new JFileChooser();
+					FileNameExtensionFilter filter = new FileNameExtensionFilter(
+							"*.jpg, *.jpeg", "jpg", "jpeg");
+					chooser.setFileFilter(filter);
+					int returnVal = chooser.showOpenDialog(panel);
+					if (returnVal == JFileChooser.APPROVE_OPTION) {
+						jwatermark.ImageWatermark.markImage(new File(sourceImagePath.getText()), new File(watermarkPath.getText()), chooser.getSelectedFile(), jwatermark.Constant.MARK_RIGHT_BOTTOM);
+					}
+				} else {
+					
+				}
+			}
+		});
 
 		JComboBox watermarkTypeCombo = new JComboBox(new DefaultComboBoxModel(new String[]{"Repeat","Left Top","Right Top","Center","Left Bottom","Rigth Bottom"}));
 
 
-		JPanel panel = new javax.swing.JPanel();
+		panel = new javax.swing.JPanel();
 		
 		GroupLayout layout = new GroupLayout(panel);
 		layout.setAutoCreateGaps(true);
@@ -67,9 +121,9 @@ public class ImageWatermarkGuiSwing extends JFrame {
 						.addComponent(watermarkImageLabel)
 						.addComponent(watermarkTypeLabel))
 				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-						.addComponent(sourceImagePath)
-						.addComponent(watermarkPath)
-						.addComponent(watermarkTypeCombo)
+						.addComponent(sourceImagePath, 330, GroupLayout.DEFAULT_SIZE, 600)
+						.addComponent(watermarkPath, 330, GroupLayout.DEFAULT_SIZE, 600)
+						.addComponent(watermarkTypeCombo, 330, GroupLayout.DEFAULT_SIZE, 600)
 						.addComponent(saveButton))
 				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
 						.addComponent(browseSourceButton)
